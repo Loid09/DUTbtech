454bf062d3b3584b2aee52b89d41017c671e105a
Loid09 <briceahissou@gmail.com>
2023-05-10 15:37:05 +0100

    modification de la bd

--- dut.sql
+++ dut.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : lun. 08 mai 2023 à 15:05
+-- Généré le : mer. 10 mai 2023 à 14:54
 -- Version du serveur : 10.4.28-MariaDB
 -- Version de PHP : 8.2.4
 
@@ -63,20 +63,26 @@ CREATE TABLE `etudiant` (
   `mail` varchar(255) NOT NULL,
   `nom_ecole` varchar(255) NOT NULL,
   `emplacement` varchar(255) NOT NULL,
-  `jeux` enum('digitalMiss','hackathon','','') NOT NULL
+  `hackathon` tinyint(1) DEFAULT 0,
+  `digitalMiss` tinyint(1) DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Déchargement des données de la table `etudiant`
 --
 
-INSERT INTO `etudiant` (`id`, `nom`, `prenom`, `tel`, `mail`, `nom_ecole`, `emplacement`, `jeux`) VALUES
-(8, 'RODIAS FIRSDYNO SEMEVO GOHOUE', 'jgk', '+22997933988', 'gohouerodias@gmail.com', 'RODIAS SEMEVO GOHOUE', 'https://baraboucle.com/', 'hackathon'),
-(9, 'RODIAS FIRSDYNO SEMEVO GOHOUE', 'jgk', '+22997933988', 'gohouerodias@gmail.com', 'RODIAS SEMEVO GOHOUE', 'https://baraboucle.com/', 'hackathon'),
-(10, 'RODIAS SEMEVO GOHOUE', 'fgjhk', '5628629+', 'gohouerodias@gmail.com', 'RODIAS GOHOUE', 'https://baraboucle.com/', 'digitalMiss'),
-(11, 'azert', 'gkljkl', '54386', 'riogoereo@gmail.com', 'dirgorurg', 'uiert', 'hackathon'),
-(12, 'azert', 'gkljkl', '54386', 'riogoereo@gmail.com', 'dirgorurg', 'uiert', 'hackathon'),
-(13, 'RODIAS FIRSDYNO SEMEVO GOHOUE', 'sqdsdqd', '+22997933988', 'gohouerodias@gmail.com', 'Alex Qweme', 'https://baraboucle.com/', 'digitalMiss');
+INSERT INTO `etudiant` (`id`, `nom`, `prenom`, `tel`, `mail`, `nom_ecole`, `emplacement`, `hackathon`, `digitalMiss`) VALUES
+(8, 'RODIAS FIRSDYNO SEMEVO GOHOUE', 'jgk', '+22997933988', 'gohouerodias@gmail.com', 'RODIAS SEMEVO GOHOUE', 'https://baraboucle.com/', 0, 0),
+(9, 'RODIAS FIRSDYNO SEMEVO GOHOUE', 'jgk', '+22997933988', 'gohouerodias@gmail.com', 'RODIAS SEMEVO GOHOUE', 'https://baraboucle.com/', 0, 0),
+(10, 'RODIAS SEMEVO GOHOUE', 'fgjhk', '5628629+', 'gohouerodias@gmail.com', 'RODIAS GOHOUE', 'https://baraboucle.com/', 0, 0),
+(11, 'azert', 'gkljkl', '54386', 'riogoereo@gmail.com', 'dirgorurg', 'uiert', 0, 0),
+(12, 'azert', 'gkljkl', '54386', 'riogoereo@gmail.com', 'dirgorurg', 'uiert', 0, 0),
+(13, 'RODIAS FIRSDYNO SEMEVO GOHOUE', 'sqdsdqd', '+22997933988', 'gohouerodias@gmail.com', 'Alex Qweme', 'https://baraboucle.com/', 0, 0),
+(14, 'RODIAS FIRSDYNO SEMEVO GOHOUE', 'RODIAS ', '+22997933988', 'gohouerodias@gmail.com', 'ODIAS OHOUE', 'https://baraboucle.com/', 0, NULL),
+(15, 'RODIAS FIRSDYNO SEMEVO GOHOUE', 'RODIAS ', '+22997933988', 'gohouerodias@gmail.com', 'ODIAS OHOUE', 'https://baraboucle.com/', 1, 0),
+(16, 'RODIAS FIRSDYNO SEMEVO GOHOUE', 'RODIAS', '+22997933988', 'gohouerodias@gmail.com', 'Alex Qweme', 'https://baraboucle.com/', 1, 0),
+(17, 'RODIAS FIRSDYNO SEMEVO GOHOUE', 'RODIAS ', '+22997933988', 'gohouerodias@gmail.com', 'RODIAS GOHOUE', 'https://baraboucle.com/', 1, 0),
+(18, 'RODIAS FIRSDYNO SEMEVO GOHOUE', 'sqdsd', '+22997933988', 'gohouerodias@gmail.com', 'Alex Qweme', 'https://baraboucle.com/', 1, 0);
 
 -- --------------------------------------------------------
 
@@ -103,7 +109,13 @@ CREATE TABLE `universite` (
 --
 
 INSERT INTO `universite` (`id`, `nom_universite`, `nom_ecole`, `adresse`, `tel`, `mail`, `participer`, `raison`, `reserver`, `periode_meilleur`, `autre_raison`) VALUES
-(1, 'RODIAS', 'GOHOUE', '', '+22997933988', 'gohouerodias@gmail.com', 0, '', 0, '', '');
+(1, 'RODIAS', 'GOHOUE', '', '+22997933988', 'gohouerodias@gmail.com', 0, '', 0, '', ''),
+(2, 'RODIAS', 'GOHOUE', '', '+22997933988', 'gohouerodias@gmail.com', 0, '', 0, '', ''),
+(3, 'RODIAS', 'GOHOUE', '', '+22997933988', 'gohouerodias@gmail.com', 1, '', 0, '', ''),
+(4, 'RODIAS', 'GOHOUE', '', '+22997933988', 'gohouerodias@gmail.com', 0, 'raison1', 1, '', ''),
+(5, 'RODIAS', 'GOHOUE', '', '+22997933988', 'gohouerodias@gmail.com', 0, 'Période non propice pour cause de préparation aux examens de fin de semestre', 1, '124', ''),
+(6, '', 'RODIAS FIRSDYNO SEMEVO GOHOUE', 'LOKOSSA-BENIN', '+22997933988', 'gohouerodias@gmail.com', 0, 'raison1', 1, '', ''),
+(7, '', 'RODIAS FIRSDYNO SEMEVO GOHOUE', 'LOKOSSA-BENIN', '+22997933988', 'gohouerodias@gmail.com', 1, '', 0, '', '');
 
 --
 -- Index pour les tables déchargées
@@ -141,13 +153,13 @@ ALTER TABLE `entreprise`
 -- AUTO_INCREMENT pour la table `etudiant`
 --
 ALTER TABLE `etudiant`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT pour la table `universite`
 --
 ALTER TABLE `universite`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
